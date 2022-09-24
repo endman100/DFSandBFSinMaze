60647b7654c1018366291a78f4320ec241a564b1
endman100 <endman246738@gmail.com>
2022-09-24 21:45:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ def getConnectToStartSet(maze, canBreakWall, startSet, mazeW):
             if(targetPath[0] in startSet): returnPath.append(targetPath[1])
             if(targetPath[1] in startSet): returnPath.append(targetPath[0])
     return returnWall, returnPath
-def getMaze(h, w):
+def getMaze(h, w, preview=False):
     # ======初始化迷宮======
     mazeH, mazeW = h * 2 + 1, w * 2 + 1
-    maze = [0] * (mazeH * mazeW)
+    maze = np.zeros((mazeH * mazeW))
     for i in range(1, mazeH, 2):
         for j in range(1, mazeW, 2):
             maze[i * mazeW + j] = 1
@@ -83,40 +83,58 @@ def getMaze(h, w):
         maze[targetWall] = 1
         targetPath = connectPath[randIndex]
         startSet.append(targetPath)
+        if(preview):
+            drawMaze(maze, h, w, 500, 500, preview=True)
 
     returnMaze = np.zeros((mazeH, mazeW))
     for i in range(0, mazeH):
         for j in range(0, mazeW):
             returnMaze[i][j] = maze[i * mazeW + j] if maze[i * mazeW + j] >= 0 else 0
     return returnMaze
+def drawMaze(maze, h, w, mazeH, mazeW, preview=False):
+    if(maze.ndim == 1):
+        newMazeH, newMazeW = h * 2 + 1, w * 2 + 1
+        newMaze = np.zeros((newMazeH, newMazeW))
+        for i in range(0, newMazeH):
+            for j in range(0, newMazeW):
+                newMaze[i][j] = maze[i * newMazeW + j] if maze[i * newMazeW + j] >= 0 else 0
+    else:
+        newMaze = maze
 
-h, w = 30, 30
+    sizeH, sizeW = mazeH/(h+2), mazeW/(w+2)
+    mazeImage = np.zeros((mazeH, mazeW, 3), dtype=np.uint8)+255
+    for i in range(h):
+        for j in range(w):
+            if(newMaze[i * 2, j * 2 + 1] == 0): #up
+                x1, y1, x2, y2 = sizeH * i, sizeW * j, sizeH * (i), sizeW * (j+1)
+                x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
+                cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
+            if(newMaze[i * 2 + 1, j * 2] == 0): #left
+                x1, y1, x2, y2 = sizeH * i, sizeW * j, sizeH * (i+1), sizeW * (j)
+                x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
+                cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
+            if(newMaze[i * 2 + 1, j * 2 + 2] == 0): #right
+                x1, y1, x2, y2 = sizeH * i, sizeW * (j+1), sizeH * (i+1), sizeW * (j+1)
+                x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
+                cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
+            if(newMaze[i * 2 + 2, j * 2 + 1] == 0): #down
+                x1, y1, x2, y2 = sizeH * (i+1), sizeW * j, sizeH * (i+1), sizeW * (j+1)
+                x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
+                cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
+
+    cv2.imshow("asd", mazeImage)
+    if(preview):
+        cv2.waitKey(1)
+    else:
+        cv2.waitKey(0)
+
+
+h, w = 50, 50
 mazeH, mazeW = 500, 500
-sizeH, sizeW = mazeH/(h+2), mazeW/(w+2)
-maze = getMaze(h, w)
+maze = getMaze(h, w, preview=True)
+drawMaze(maze, h, w, mazeH, mazeW)
 print(maze)
-mazeImage = np.zeros((mazeH, mazeW, 3), dtype=np.uint8)+255
-for i in range(h):
-    for j in range(w):
-        if(maze[i * 2, j * 2 + 1] == 0): #up
-            x1, y1, x2, y2 = sizeH * i, sizeW * j, sizeH * (i), sizeW * (j+1)
-            x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
-            cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
-        if(maze[i * 2 + 1, j * 2] == 0): #left
-            x1, y1, x2, y2 = sizeH * i, sizeW * j, sizeH * (i+1), sizeW * (j)
-            x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
-            cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
-        if(maze[i * 2 + 1, j * 2 + 2] == 0): #right
-            x1, y1, x2, y2 = sizeH * i, sizeW * (j+1), sizeH * (i+1), sizeW * (j+1)
-            x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
-            cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
-        if(maze[i * 2 + 2, j * 2 + 1] == 0): #down
-            x1, y1, x2, y2 = sizeH * (i+1), sizeW * j, sizeH * (i+1), sizeW * (j+1)
-            x1, y1, x2, y2 = x1 + sizeH, y1 + sizeW, x2 + sizeH, y2 + sizeW
-            cv2.line(mazeImage, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 1)
 
-cv2.imshow("asd", mazeImage)
-cv2.waitKey(0)
 # #Rearrang the color channel
 # b,g,r = cv2.split(img)
 # img = cv2.merge((r,g,b))
